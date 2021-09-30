103a819517caf6ae056b62373868aa5cbe34fcac
“Apoorv <tiwari.apoorv1316@gmail.com>
2021-09-30 21:18:31 +0530

    initial commit

---
+++ portfolio.js
@@ -0,0 +1,209 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "Swati Singh",
+  title: "Give me a beer 🍺",
+  description: "Engineer by profession & Poet by passion ❤️",
+  resumeLink: "https://cv.hanzla.ga",
+};
+
+export const openSource = {
+  githubUserName: "1hanzla100",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  instagram: "https://www.instagram.com/swatisingh2800/",
+  linkedin: "https://www.linkedin.com/in/swati-singh-aa77991b4/",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "Engineer by profession & Poet by passion ❤️",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+    ),
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "flutter",
+      fontAwesomeClassname: "logos:flutter",
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "vscode-icons:file-type-swift",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "vscode-icons:file-type-sql",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "logos:aws",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "logos:firebase",
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "logos:python",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "logos:docker-icon",
+    },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "AutoCAD", //Insert stack or technology you have experience in
+    progressPercentage: "90", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "PRO E",
+    progressPercentage: "70",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "60",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "Arya College of Engineering & IT",
+    subHeader: "Bachelor in Technology in Mechanical engineering",
+    duration: "July 2018 - June 2022",
+    desc: "Participated in the research of XXX and published 3 papers.",
+    descBullets: [
+      "Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ],
+  },
+  {
+    schoolName: "SKD Academy",
+    subHeader: "Senior Secondary",
+    duration: "2018",
+    desc: "Participated in the research of XXX and published 3 papers.",
+    descBullets: [
+      "Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    ],
+  },
+  {
+    schoolName: "City Montessori School, Lucknow",
+    subHeader: "Secondary",
+    duration: "2016",
+    desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+    descBullets: ["Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit"],
+  },
+];
+
+export const experience = [
+	{
+		role: "Software Engineer",
+		company: "Google",
+		companylogo: "/img/icons/common/google.svg",
+		date: "June 2018 – Present",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		descBullets: [
+			"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		],
+	},
+	{
+		role: "Front-End Developer",
+		company: "Github",
+		companylogo: "/img/icons/common/github.svg",
+		date: "May 2017 – May 2018",
+		desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+	{
+		role: "Software Engineer Intern",
+		company: "Airbnb",
+		companylogo: "/img/icons/common/airbnbLogo.png",
+		date: "Jan 2015 – Sep 2015",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	},
+];
+
+export const projects = [
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		link: "https://example.com",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1hanzla100",
+	},
+	{
+		name: "lorem ipsum",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+		github: "https://github.com/1hanzla100",
+		link: "https://example.com",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Hassan Mehmood",
+		feedback:
+			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
+	},
+	{
+		name: "Maheen Altaf",
+		feedback:
+			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+	},
+];
